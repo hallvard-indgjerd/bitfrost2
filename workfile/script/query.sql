82683cf2ea2d1be58a10dee129bb386e1a7a8102
Giuseppe Naponiello <beppenapo@arc-team.com>
2024-10-02 22:40:33 +0200

    api json-ld v1

--- workfile/script/query.sql
+++ workfile/script/query.sql
@@ -1,12 +1,33 @@
-SELECT m.id, m.name, m.description, m.status, o.thumbnail, o.create_at
-FROM model m
-inner JOIN (
-    SELECT o1.*
-    FROM model_object o1
-    INNER JOIN (
-        SELECT model, MIN(id) AS obj_id
-        FROM model_object
-        GROUP BY model
-    ) o2 ON o1.id = o2.obj_id
-) o ON m.id = o.model
-where o.author = 21;
+select 
+ concat('https://dyncolldev.ht.lu.se/plus/artifact_view.php?item=',artifact.id) as link,
+ model.doi,
+ artifact.name,
+ class.value as class,
+ typology.value as specific_class,
+ artifact.type as typology,
+ artifact.start as start_chronology,
+ artifact.end as end_chronology,
+ conservation.value as conservation_state,
+ list_object_condition.value as object_condition,
+ if(artifact.is_museum_copy = 1, 'true', 'false') as is_museum_copy,
+ artifact.description,
+ artifact.notes,
+ concat(auth.first_name,' ',auth.last_name) author,
+ o.name owner,
+ artifact.inventory,
+ concat(license.license,' (',license.acronym,')') as license,
+ cast(artifact.created_at as date) created_at,
+ cast(artifact.last_update as date) last_update
+from artifact
+inner join artifact_model am on am.artifact = artifact.id
+inner join model on am.model = model.id
+inner join institution o on artifact.owner = o.id
+inner join user on artifact.author = user.id
+inner join person auth on user.person = auth.id
+inner join license on artifact.license = license.id
+inner join list_category_class class on artifact.category_class = class.id
+inner join list_category_specs typology on artifact.category_specs = typology.id
+inner join list_conservation_state conservation on artifact.conservation_state = conservation.id
+inner join list_object_condition on artifact.object_condition = list_object_condition.id
+where artifact.status = 2
+order by artifact.id asc;
