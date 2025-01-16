88d6cdccff0ef05d8dd82555f90aaa8b71dde869
Hallvard Indgjerd <hallvard.in@gmail.com>
2025-01-16 18:30:32 +0100

    Changing title and logo to match UiO BItFROST

--- assets/meta.php
+++ assets/meta.php
@@ -3,12 +3,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5.0">
 <meta name="author" content="Giuseppe Naponiello-DarkLab-Lund University" >
 <meta name="robots" content="INDEX,FOLLOW" >
-<meta name="description" content="Archaeological Dynamic Collections" >
+<meta name="description" content="BItFROST 2.0, based on Archaeological Dynamic Collections by DarkLab at Lund University" >
 <meta name="keywords" content="" >
-<meta name="title" content="Archaeological Dynamic Collections">
+<meta name="title" content="BItFROST 2.0">
 <meta name="theme-color" content="#4d4c44"/>
 <!-- <link rel="manifest" href="manifest.json"> -->
 <link rel="shortcut icon" href="img/ico/favicon.png">
 <!-- <link rel="apple-touch-icon" href="img/icons/app-icon-192x192.png"> -->
-<title>Archaeological Dynamic Collections</title>
+<title>BItFROST 2.0</title>
 <?php require("css.html"); ?>
+
